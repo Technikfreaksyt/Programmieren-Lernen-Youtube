01c5b989d94ee0c49d7603a3e0c1a1bd6f5da9ef
Technik freaks <106913276+Technikfreaksyt@users.noreply.github.com>
2023-05-27 12:37:58 +0200

    added script for video

---
+++ Discord bot-slash_Commands - - Video vom 25.05.2023.py
@@ -0,0 +1,44 @@
+import discord
+from discord import app_commands
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+MYGUILD = discord.Object(id=984482993336905738)
+
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MYGUILD)
+        await self.tree.sync(guild=MYGUILD)
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+    
+    activity = discord.Game(name="/help", type=1)
+
+    await client.change_presence(status=discord.Status.online, activity=activity)
+
+
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Sagt Hallo"""
+    
+    
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+
+
+client.run("OTg1MDg3MzAyNjI5OTkwNDEw.G2EFdF.sototzndZky5LRYgf7D6FQo1p5IO7ANKWoB90o")
